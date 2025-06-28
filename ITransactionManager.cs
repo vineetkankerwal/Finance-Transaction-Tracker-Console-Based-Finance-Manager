dd8c759b1dc6965c332096ce640109c783e6c373
VINEET KANKERWAL <90712707+vineetkankerwal@users.noreply.github.com>
2025-06-28 21:35:56 +0530

    Update ITransactionManager.cs

--- ITransactionManager.cs
+++ ITransactionManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Vineet_CSharp_Test
+namespace Finane_Transaction_Manager
 {
     public interface ITransactionManager
     {
